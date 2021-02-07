d95723b64676d4c63d66c3dfd966903288b9fda1
neel229 <device.neel@gmail.com>
2021-02-07 22:04:32 +0530

    CRUD for currency_rate table added

---
+++ pkg/db/query/currency_rate.sql
@@ -0,0 +1,20 @@
+-- name: CreateCurrencyRate :one
+INSERT INTO currency_rate (
+  currency_id,
+  base_currency_id,
+  rate
+) VALUES (
+  $1, $2, $3
+)
+RETURNING *;
+
+-- name: GetCurrencyRate :one
+SELECT * FROM currency_rate
+WHERE id = $1
+LIMIT 1;
+
+-- name: UpdateCurrencyRate :exec
+UPDATE currency_rate
+SET rate = $3
+WHERE currency_id = $1 
+AND base_currency_id = $2;
