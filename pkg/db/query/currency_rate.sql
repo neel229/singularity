572d77ebf96e5ef71ccdeab13de6d525a9675abe
neel229 <device.neel@gmail.com>
2021-02-10 11:09:20 +0530

    Refactored

--- pkg/db/query/currency_rate.sql
+++ pkg/db/query/currency_rate.sql
@@ -1,19 +1,14 @@
 -- name: CreateCurrencyRate :one
-INSERT INTO currency_rate (
-  currency_id,
-  base_currency_id,
-  rate
-) VALUES (
-  $1, $2, $3
-)
+INSERT INTO currency_rate (currency_id, base_currency_id, rate)
+VALUES ($1, $2, $3)
 RETURNING *;
-
 -- name: GetCurrencyRate :one
-SELECT * FROM currency_rate
+SELECT *
+FROM currency_rate
 WHERE id = $1
 LIMIT 1;
-
 -- name: UpdateCurrencyRate :exec
 UPDATE currency_rate
 SET rate = $2
-WHERE id = $1;
+WHERE id = $1
+RETURNING *;
