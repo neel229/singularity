82b609f62b6ed66fcc828dc9fffb5d0f862d8294
neel229 <device.neel@gmail.com>
2021-02-23 11:53:27 +0530

    add go-chi to go.mod

--- go.mod
+++ go.mod
@@ -3,12 +3,7 @@ module github.com/neel229/singularity
 go 1.15
 
 require (
-	github.com/andybalholm/brotli v1.0.1 // indirect
-	github.com/go-chi/chi v1.5.3 // indirect
-	github.com/gofiber/fiber/v2 v2.5.0
-	github.com/klauspost/compress v1.11.7 // indirect
+	github.com/go-chi/chi v1.5.3
 	github.com/lib/pq v1.9.0
 	github.com/stretchr/testify v1.7.0
-	github.com/valyala/fasthttp v1.21.0 // indirect
-	golang.org/x/sys v0.0.0-20210220050731-9a76102bfb43 // indirect
 )
--- pkg/api/routes.go
+++ pkg/api/routes.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 )
@@ -9,44 +10,9 @@ import (
 // corresponding handler functions
 func (s *Server) SetRoutes() {
 	s.r.Get("/home", homepage)
+	s.r.Post("/create/creator", s.CreateCreator(context.Background()))
 }
 
 func homepage(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("This is the homepage")
 }
-
-// // Logic for creating a creator account
-// // and corresponding stock
-// type createCreatorRequest struct {
-// 	FirstName           string `json:"first_name" validate:"required"`
-// 	LastName            string `json:"last_name" validate:"required"`
-// 	UserName            string `json:"user_name" validate:"required"`
-// 	Email               string `json:"email" validate:"required, email"`
-// 	Password            string `json:"password" validate:"required"`
-// 	PreferredCurrencyID int32  `json:"preferred_currency_id" validate:"required"`
-// 	Ticker              string `json:"ticker" validate:"required"`
-// 	Details             string `json:"details" validate:"required"`
-// }
-
-// func createCreator(c *fiber.Ctx) error {
-// 	req := new(createCreatorRequest)
-// 	if err := c.BodyParser(req); err != nil {
-// 		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
-// 	}
-
-// 	arg := db.StockCreationTxParams{
-// 		FirstName:           req.FirstName,
-// 		LastName:            req.LastName,
-// 		UserName:            req.UserName,
-// 		Email:               req.Email,
-// 		Password:            req.Password,
-// 		PreferredCurrencyID: req.PreferredCurrencyID,
-// 		Ticker:              req.Ticker,
-// 		Details:             req.Details,
-// 	}
-
-// 	s := Server{
-// 		store: NewStore(),
-// 	}
-// 	return nil
-// }
