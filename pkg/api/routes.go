60b6ac54b6255ca95582df6af1faba27d66bda4f
neel229 <device.neel@gmail.com>
2021-02-24 10:15:31 +0530

    refactored currency routes to use chi.Router methods

--- pkg/api/routes.go
+++ pkg/api/routes.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
@@ -19,11 +20,17 @@ func (s *Server) SetRoutes() {
 // CurrencyRoutes sets the routes
 // respect to currencies
 func (s *Server) CurrencyRoutes() {
-	s.r.Get("/currency/{id}", s.GetCurrency(s.ctx))
-	s.r.Get("/currencies", s.ListCurrencies(s.ctx))
-	s.r.Post("/create/currency", s.CreateCurrency(s.ctx))
-	s.r.Put("/update/currency/{id}", s.UpdateCurrency(s.ctx))
-	s.r.Get("/currency/{id}", s.GetCurrency(s.ctx))
+	s.r.Route("/currency", func(r chi.Router) {
+		r.Get("/", s.ListCurrencies(s.ctx))
+		r.Post("/", s.CreateCurrency(s.ctx))
+
+		// Subroutes for currency
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/", s.GetCurrency(s.ctx))
+			r.Put("/", s.UpdateCurrency(s.ctx))
+			r.Delete("/", s.DeleteCurrency(s.ctx))
+		})
+	})
 }
 
 func homepage(w http.ResponseWriter, r *http.Request) {
